a50478cb2d3d551c20a06684ce03c7834d567838
Jonathon Carl <jac16@williams.edu>
2022-12-08 21:47:57 -0500

    added parser documentation

--- code/backend/Groupify/Parser.fs
+++ code/backend/Groupify/Parser.fs
@@ -8,16 +8,35 @@ open AST
  *)
 let pexpr,pexprImpl = recparser()
 
+(*
+ * Parse a negative number.
+ *)
 let pnegative = pseq (pchar '-') (pmany1 pdigit) (fun(negative, nums) -> negative::nums) <!> "pnegative"
 
+(*
+ * Parse a positive number.
+ *)
 let ppositive = pmany1 pdigit <!> "ppositive"
 
+(*
+ * Parse the integers.
+ *)
 let pintegers = pchar 'Z' |>> string |>> Integers <!> "pintegers"
  
+(*
+ * Parse the rational numbers.
+ *)
 let prationals = pchar 'Q' |>> string |>> Rationals <!>"prationals"
 
+(*
+ * Parse the real numbers.
+ *)
 let preal = pchar 'R' |>> string |>> Reals <!> "preal"
 
+
+(*
+ * Parse the complex numbers.
+ *)
 let pcomplex = pchar 'C' |>> string |>> Complex <!> "pcomplex"
 
 (*
@@ -26,28 +45,42 @@ let pcomplex = pchar 'C' |>> string |>> Complex <!> "pcomplex"
 let pnum = pnegative <|> ppositive |>> stringify |>> float |>> Num <!> "pnum"
 
 (*
- * Helper parser to read first two signs of addition modulo operation.
+ * Parse the addition modulo operation.
  *)
 let fmodoper = pseq (pchar '+') (pchar '%') (fun (fop,sop) -> string fop,string sop) <!> "fmodoper"
 
-(*
- * Addition modulo operation parser.
- *)
 let modoper = pseq fmodoper (pdigit) (fun (fop, numOp) -> 
                                     let a,b = fop
                                     Operation(a + b + string(numOp)))
                                      <!> "modoper"
-
+(*
+ * Parse the addition operation.
+ *)
 let aoper = pchar '+' |>> string |>> Operation <!> "aoper"
 
+(*
+ * Parse the subtraction operation.
+ *)
 let soper = pchar '-' |>> string |>> Operation <!> "soper"
 
+(*
+ * Parse the multiplication operation.
+ *)
 let moper = pchar '*' |>> string |>> Operation <!> "moper"
 
+(*
+ * Parse the division operation.
+ *)
 let doper = pchar '/' |>> string |>> Operation <!> "doper"
 
+(*
+ * Parse the operation.
+ *)
 let poper = modoper <|> aoper <|> soper <|> moper <|> doper <!> "poper"
 
+(*
+ * Parse a set of numbers.
+ *)
 let pnums = (pseq (pmany1 (pleft (pnum) (pchar ','))) (pnum) (fun (nums, num) -> nums @ [num] )) |>> Numbers <!> "pnums"
 
 (*
